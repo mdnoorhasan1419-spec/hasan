c0c0825b2c5a06db667274a1026f8a0e85616273
mdnoorhasan1419-spec <mdnoorhasan1419@gmail.com>
2026-09-06 06:52:09 +0000

    variable

---
+++ hasan1/variable.js
@@ -0,0 +1,12 @@
+const acconteID= "1419"
+let accontEmail="hasan@gmail,com"
+var accontpassword="2099"
+accontcity="jalley"
+
+// accontEmail="noor@gmail,com"
+//  accontpassword="2000"
+// accontcity="jale"
+
+// console.table([acconteID,accontEmail,accontpassword,accontcity]);
+
+console.log(accontpassword);
