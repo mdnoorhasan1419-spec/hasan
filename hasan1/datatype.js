3b404f27898ef2e053657a3354664481874cd535
mdnoorhasan1419-spec <mdnoorhasan1419@gmail.com>
2026-09-06 06:51:23 +0000

    container config com datatype

---
+++ hasan1/datatype.js
@@ -0,0 +1,19 @@
+ let datatype= 
+// Number => 2 to 53
+// BigInt
+// String ""
+// boolean => true/false
+// null 
+// undefined
+// Symbol=> unique
+
+
+
+
+console.log(typeof hasan); //undefined
+console.log(typeof 5667);  //Number
+console.log(typeof null);  //objects
+console.log(typeof undefined);   //undefined
+console.log(typeof "noor");  //string
+console.log(typeof Symbol);    //function
+// console.table([hasan,5667,null,undefined,"noor",Symbol]);
