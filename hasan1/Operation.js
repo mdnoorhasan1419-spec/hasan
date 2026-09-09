abaedf74c2034b24e527c49c5bb927299626fc9d
mdnoorhasan1419-spec <mdnoorhasan1419@gmail.com>
2026-09-09 09:25:35 +0000

    Operation

---
+++ hasan1/Operation.js
@@ -0,0 +1,34 @@
+let value= 3
+let negvalue= -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/5);
+
+let str1="noor "
+let str2="hasan"
+let str3=str1+str2
+console.log(str3);
+
+console.log("1" +2);
+//A string plus a number converts the number to a string
+console.log(1+"2");
+//A number plus a string converts the number to a string.
+console.log("1" + 2 + 2);
+//Evaluates left-to-right. First, "1" + 2 becomes "12". Then, "12" + 2 becomes "122".
+console.log(1 + 2 + "2");
+//Evaluates left-to-right. First, the numbers 1 + 2 add up to 3. Then, the number 3 joins the string "2" to become "32".
+console.log(2456);
+
+console.log(+true);
+
+let gamcounter=100
+//gamcounter++;  Post-increment 
+++gamcounter;//Pre-increment
+console.log(gamcounter);
+
+
