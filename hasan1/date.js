8b3f248300025872471a936e3d1bb1ad011bf2c2
mdnoorhasan1419-spec <mdnoorhasan1419@gmail.com>
2026-09-12 04:07:56 +0000

    date

---
+++ hasan1/date.js
@@ -0,0 +1,37 @@
+// Method                 	Example
+// getFullYear()		date.getFullYear()
+// getMonth()	     	date.getMonth()
+// getDate()	     	date.getDate()
+// getDay()	    	    date.getDay()
+// getHours()	     	date.getHours()
+// getMinutes()		    date.getMinutes()
+// getSeconds()		    date.getSeconds()
+// getMilliseconds()	date.getMilliseconds()
+// getTime()       	    date.getTime()
+
+let MyDate = new Date();
+console.log( MyDate.toString());
+console.log( MyDate.toDateString());
+console.log( MyDate.toLocaleString());
+console.log(typeof MyDate);
+let createDate = new Date(2026,8,12);
+console.log(createDate.toDateString());
+console.log(createDate.toLocaleString());
+let creteDate = new Date("12-9-2026");
+console.log(creteDate.toLocaleString());
+Date()
+
+console.log (new Date().getFullYear());
+console.log (new Date().getMonth());
+console.log (new Date().getDate());     	
+console.log (new Date().getDay()); 	   
+console.log (new Date().getHours()); 
+console.log (new Date().getMinutes()); 
+console.log (new Date().getSeconds()); 
+console.log (new Date().getMilliseconds()); 
+console.log (new Date().getTime());
+	    	  
+	     	
+		   
+	
+	
