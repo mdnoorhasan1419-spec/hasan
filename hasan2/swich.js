67e0a07728dff5d2d72ed4d6e07f4491b9ebb1e8
mdnoorhasan1419-spec <mdnoorhasan1419@gmail.com>
2026-09-21 12:03:36 +0000

    swich

---
+++ hasan2/swich.js
@@ -0,0 +1,71 @@
+// Syntax / Keyword         Kaam                                      Example
+
+// switch                   Ek value ko multiple cases se compare     switch(day)
+
+// case                     Specific value match karta hai             case 1:
+
+// break                    Matching case ke baad switch rokta hai    break;
+
+// default                  Koi case match na ho to chalta hai         default:
+
+// switch...case             Multiple fixed values check karta hai      switch(choice)
+
+let subject = "g";
+
+switch (subject) {
+    case "a":
+        console.log("opreting system");
+        break;
+
+    case "b":
+        console.log("data structure");
+        break;
+
+    case "c":
+        console.log("python");
+        break;
+    case"d":
+        console.log("networking sustem");
+        break;
+    case "e":
+        console.log("rdnms");
+        break;
+    case "f":
+        console.log("multimedia");
+        break;
+    case "g":
+        console.log("number system");
+        break;
+    case "h":
+        console.log("communication skills");
+        break;
+    case "i":
+        console.log("organizational behaviour");
+        break;
+    case "j":
+        console.log("lab programing");
+        break;
+
+    default:
+        console.log("Invalid subject");
+}
+
+// let day = 2;
+
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+
+//     default:
+//         console.log("Invalid day");
+// }
+
