767478839646d9a86cdb11cb525d159f7141383b
mdnoorhasan1419-spec <mdnoorhasan1419@gmail.com>
2026-09-21 18:17:04 +0000

    loops

--- hasan2/loops.js
+++ hasan2/loops.js
@@ -1,17 +1,11 @@
 // Loop / Keyword           Kaam                                      Example
 
 // for loop                 Fixed number of times repeat karta hai    for(let i=0; i<5; i++)
-
 // while loop               Jab tak condition true ho, chalta hai     while(i < 5)
-
 // do...while               Pehle ek baar code chalata hai             do {...} while(i < 5)
-
 // for...of                 Array/string ki values deta hai            for(let x of arr)
-
 // for...in                 Object ki keys/index deta hai              for(let x in user)
-
 // break                    Loop ko turant stop karta hai              if(i == 3) break;
-
 // continue                 Current iteration skip karta hai            if(i == 3) continue;
 
 for (let i = 0; i <=10; i++) {
